a4b6921c740d3651fae2f52835c01a255cc4bfac
sangchu <didi1484@gmail.com>
2024-08-07 13:43:01 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/stayforyou/StayForYouApplication.java
@@ -0,0 +1,13 @@
+package com.stayforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayForYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayForYouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stayforyou/StayForYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayForYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
