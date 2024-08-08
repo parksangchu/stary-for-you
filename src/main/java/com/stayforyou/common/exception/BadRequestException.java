85502ff7361d383085fb028c94c6fb9cb9fdb65d
sangchu <didi1484@gmail.com>
2024-08-08 13:22:55 +0900

    fix: 공용 예외처리 구현

---
+++ src/main/java/com/stayforyou/common/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.stayforyou.common.exception;
+
+public class BadRequestException extends BizException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/stayforyou/common/exception/BizException.java
@@ -0,0 +1,7 @@
+package com.stayforyou.common.exception;
+
+public class BizException extends RuntimeException {
+    public BizException(String message) {
+        super(message);
+    }
+}
