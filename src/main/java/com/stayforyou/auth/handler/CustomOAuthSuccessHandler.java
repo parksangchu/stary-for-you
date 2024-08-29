c42f8783cd3f957a950793fd99ef05f16500e31e
sangchu <didi1484@gmail.com>
2024-08-29 14:42:16 +0900

    fix: oauth2 redirectUrl 상수 -> 설정으로 변경

--- src/main/java/com/stayforyou/auth/handler/CustomOAuthSuccessHandler.java
+++ src/main/java/com/stayforyou/auth/handler/CustomOAuthSuccessHandler.java
@@ -5,21 +5,27 @@ import com.stayforyou.auth.util.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@RequiredArgsConstructor
 @Component
 @Slf4j
 public class CustomOAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-    private static final String TARGET_REDIRECT_URL = "http://localhost:3000/auth/result";
+
+    private final String targetRedirectUrl;
 
     private final JwtUtil jwtUtil;
+
+    public CustomOAuthSuccessHandler(@Value("${spring.security.oauth2.redirect-uri}") String targetRedirectUrl,
+                                     JwtUtil jwtUtil) {
+        this.targetRedirectUrl = targetRedirectUrl;
+        this.jwtUtil = jwtUtil;
+    }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -28,7 +34,7 @@ public class CustomOAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHan
 
         String token = jwtUtil.generateToken(customOAuth2User.getName(), customOAuth2User.getRole());
 
-        String redirectUrl = UriComponentsBuilder.fromUriString(TARGET_REDIRECT_URL)
+        String redirectUrl = UriComponentsBuilder.fromUriString(targetRedirectUrl)
                 .queryParam(HttpHeaders.AUTHORIZATION, token)
                 .build()
                 .toUriString();
